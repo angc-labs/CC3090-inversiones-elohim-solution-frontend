35a48188bd336fae23492023c25b5115bc9e5edc
angcoder-c <angcoder.c@gmail.com>
2026-04-08 16:27:22 -0600

    feat: add shared types

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export type TProducto = {
+  idProducto: string;
+  codigoProducto: string;
+  nombreProducto: string;
+  precio: number;
+  stockActual: number;
+  descripcion?: string;
+  idMarca?: string;
+  categoriaId?: string;
+  fechaVencimiento?: string;
+  imagenPrincipal?: string;
+  fechaCreacion: string;
+  fechaActualizacion: string;
+};
